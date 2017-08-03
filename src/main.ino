cf1f9d7e57264e18a4722420ca03279a18a55d2d
Lacy Rhoades <lacyrhoades@gmail.com>
2017-08-03 11:36:36 -0400

    Initial commit

---
+++ src/main.ino
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+
+const int pinLED = 13; // register C, bit 5
+
+void setup() {
+    pinMode(pinLED, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(14, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(20, OUTPUT);
+    pinMode(21, OUTPUT);
+    pinMode(5, OUTPUT);
+
+    // Start high
+    GPIOD_PDOR = GPIOD_PDIR | 0b11111111;
+}
+
+void loop() {
+    delay(5000);
+
+    // LED ON
+    GPIOC_PDOR = GPIOC_PDIR | 0b00100000;
+
+    // FOCUS
+    GPIOD_PDOR = GPIOD_PDIR & 0b10101010;
+
+    delay(1300);
+
+    // LED OFF
+    GPIOC_PDOR = GPIOC_PDIR & 0b11011111;
+
+    // FIRE
+    // GPIOD_PDOR = GPIOD_PDIR & 0b01010101;
+
+    delay(80);
+
+    // RESET
+    GPIOD_PDOR = GPIOD_PDIR | 0b11111111;
+
+    delay(300000);
+}
